8e18530148acacab231753fab611ee75d4d49ac9
Raúl Viloria <raulcodelearning@gmail.com>
2024-12-25 10:39:30 +0100

    dinamico

--- plantillas/plantillas/views.py
+++ plantillas/plantillas/views.py
@@ -1,2 +1,9 @@
+from django.shortcuts import render
+
 def simple (request):
-    return 
+    return render(request, 'simple.html', {})
+
+def dinamico(request, name):
+    categories = ['code', 'design', 'marketing']
+    context = {'name': name, 'categories' : categories}
+    return render (request, 'dinamico.html',context )
